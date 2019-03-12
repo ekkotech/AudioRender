7d1523a96fac0d4f4dd532c8b35049a02564be18
ekkotech <andrew.coad@ekko-tech.com>
2019-03-12 14:20:05 +0900

    Initial Commit

---
+++ AudioRender/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AudioRender
+//
+//  Created by Andrew Coad on 12/03/2019.
+//  Copyright © 2019 ___EKKO-TECH LTD___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
